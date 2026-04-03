6f0bc0c0e0cbd1d37d156876e5723ae37075af58
WinterWolfVN <winterwolfvn123@gmail.com>
2026-04-03 20:55:45 +0700

    Update MainActivity.kt

--- app/src/main/java/dev/beefers/vendetta/manager/ui/activity/MainActivity.kt
+++ app/src/main/java/dev/beefers/vendetta/manager/ui/activity/MainActivity.kt
@@ -2,11 +2,13 @@ package dev.beefers.vendetta.manager.ui.activity
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import cafe.adriel.voyager.navigator.Navigator
@@ -27,17 +29,24 @@ class MainActivity : ComponentActivity() {
     private val preferences: PreferenceManager by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        try {
+            installSplashScreen()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         val version = intent.getStringExtra(Intents.Extras.VERSION)
 
-        if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf("android.permission.POST_NOTIFICATIONS"),
-                0
-            )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    0
+                )
+            }
         }
 
         if (preferences.installMethod == InstallMethod.SHIZUKU) {
@@ -62,5 +71,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
 }
