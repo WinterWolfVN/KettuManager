316e9b4840d1c117c235c1027c7c47599cb84f0d
WinterWolfVN <winterwolfvn123@gmail.com>
2026-04-03 23:08:34 +0700

    Update HttpModule.kt

--- app/src/main/java/dev/beefers/vendetta/manager/di/HttpModule.kt
+++ app/src/main/java/dev/beefers/vendetta/manager/di/HttpModule.kt
@@ -3,21 +3,31 @@ package dev.beefers.vendetta.manager.di
 import dev.beefers.vendetta.manager.network.service.HttpService
 import dev.beefers.vendetta.manager.network.service.RestService
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import java.util.concurrent.TimeUnit
 
 val httpModule = module {
 
     fun provideJson() = Json {
         ignoreUnknownKeys = true
         isLenient = true
+        encodeDefaults = true
     }
 
-    fun provideHttpClient(json: Json) = HttpClient(CIO) {
+    fun provideHttpClient(json: Json) = HttpClient(OkHttp) {
+        engine {
+            config {
+                connectTimeout(30, TimeUnit.SECONDS)
+                readTimeout(60, TimeUnit.SECONDS)
+                writeTimeout(60, TimeUnit.SECONDS)
+                retryOnConnectionFailure(true)
+            }
+        }
         install(ContentNegotiation) {
             json(json)
         }
