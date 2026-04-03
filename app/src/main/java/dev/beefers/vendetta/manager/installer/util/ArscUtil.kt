f0a77988dc0e3e4c9c706928a91a216c79fcf2f8
WinterWolfVN <winterwolfvn123@gmail.com>
2026-04-04 00:29:45 +0700

    Update ArscUtil.kt

--- app/src/main/java/dev/beefers/vendetta/manager/installer/util/ArscUtil.kt
+++ app/src/main/java/dev/beefers/vendetta/manager/installer/util/ArscUtil.kt
@@ -1,20 +1,18 @@
-// https://github.com/Aliucord/Manager/blob/main/app/src/main/kotlin/com/aliucord/manager/installer/util/ArscUtil.kt
 package dev.beefers.vendetta.manager.installer.util
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import com.github.diamondminer88.zip.ZipReader
 import com.google.devrel.gmscore.tools.apk.arsc.*
 import dev.beefers.vendetta.manager.BuildConfig
 import java.io.File
+import java.util.zip.ZipFile
 
 object ArscUtil {
-    /**
-     * Read and parse `resources.arsc` from an APK.
-     */
     fun readArsc(apk: File): BinaryResourceFile {
-        val bytes = ZipReader(apk).use { it.openEntry("resources.arsc")?.read() }
-            ?: error("APK missing resources.arsc")
+        val bytes = ZipFile(apk).use { zip ->
+            val entry = zip.getEntry("resources.arsc") ?: error("APK missing resources.arsc")
+            zip.getInputStream(entry).use { it.readBytes() }
+        }
 
         return try {
             BinaryResourceFile(bytes)
@@ -23,9 +21,6 @@ object ArscUtil {
         }
     }
 
-    /**
-     * Get the only top-level chunk in an arsc file.
-     */
     fun BinaryResourceFile.getMainArscChunk(): ResourceTableChunk {
         if (this.chunks.size > 1)
             error("More than 1 top level chunk in resources.arsc")
@@ -34,21 +29,12 @@ object ArscUtil {
             ?: error("Invalid top-level resources.arsc chunk")
     }
 
-    /**
-     * Get a singular package chunk in an arsc file.
-     */
     fun BinaryResourceFile.getPackageChunk(): PackageChunk {
-        return this.getMainArscChunk().packages.singleOrNull()
-            ?: error("resources.arsc must contain exactly 1 package chunk")
+        val mainChunk = this.getMainArscChunk()
+        return mainChunk.packages.firstOrNull()
+            ?: error("resources.arsc must contain at least 1 package chunk")
     }
 
-    /**
-     * Adds a new color resource to all configuration variants in an arsc package.
-     *
-     * @param name The new resource name.
-     * @param color The value of the new color resource.
-     * @return The resource ID of the newly added resource.
-     */
     fun PackageChunk.addColorResource(
         name: String,
         color: Color,
@@ -62,16 +48,6 @@ object ArscUtil {
         )
     }
 
-    /**
-     * Adds a new color resource to the matching configuration variants in an arsc package.
-     *
-     * @param typeName The type of the resource (ex: `mipmap`, `drawable`, etc.)
-     * @param resourceName The new resource name.
-     * @param configurations A predicate whether to add the value into a matching type chunk.
-     * @param valueType The type of the resource value.
-     * @param valueData The raw data of the resource value.
-     * @return The resource ID of the newly added resource.
-     */
     fun PackageChunk.addResource(
         typeName: String,
         resourceName: String,
@@ -79,57 +55,42 @@ object ArscUtil {
         valueType: BinaryResourceValue.Type,
         valueData: Int,
     ): BinaryResourceIdentifier {
-        // Add a new resource entry to the "type spec chunk" and,
-        // a new resource entry to all matching "type chunks"
-
-        val specChunk = this.getTypeSpecChunk(typeName)
+        val specChunk = this.getTypeSpecChunks(typeName).first()
         val typeChunks = this.getTypeChunks(typeName)
 
-        // Add a new string to the pool to be used as a key
-        val resourceNameIdx = this.keyStringPool.addString(resourceName, /* deduplicate = */ true)
+        val resourceNameIdx = this.keyStringPool.addString(resourceName)
 
-        // Add a new resource entry to the type spec chunk
-	    // HACK: Resource index returned by addResource is off by 1 in release builds due to optimizations or something
-        val resourceIdx = specChunk.addResource(/* flags = */ 0) + if (BuildConfig.DEBUG) 0 else 1
+        val resourceIdx = specChunk.addResource(0) + if (BuildConfig.DEBUG) 0 else 1
 
         for (typeChunk in typeChunks) {
-            // If no matching config, add a null entry and try next chunk
             if (!configurations(typeChunk.configuration)) {
                 typeChunk.addEntry(null)
                 continue
             }
 
             val entry = TypeChunk.Entry(
-                /* headerSize = */ 8,
-                /* flags = */ 0,
-                /* keyIndex = */ resourceNameIdx,
-                /* value = */
+                8,
+                0,
+                resourceNameIdx,
                 BinaryResourceValue(
-                    /* type = */ valueType,
-                    /* data = */ valueData,
+                    valueType,
+                    valueData,
                 ),
-                /* values = */ null, // not a complex resource
-                /* parentEntry = */ 0, // not a complex resource
-                /* parent = */ typeChunk,
+                null,
+                0,
+                typeChunk,
             )
 
             typeChunk.addEntry(entry)
         }
 
         return BinaryResourceIdentifier.create(
-            /* packageId = */ this.id,
-            /* typeId = */ specChunk.id,
-            /* entryId = */ resourceIdx,
+            this.id,
+            specChunk.id,
+            resourceIdx,
         )
     }
 
-    /**
-     * In an arsc file, for a specific resource in a configuration, get it's value.
-     *
-     * @param resourceId The target resource id.
-     * @param configurationName The target configuration variant of the resource. (ex: `anydpi-v26`, `xxhdpi`, `ldtrl-mpi`, etc.)
-     * @return The string value of the resource, which should be a file path inside the apk.
-     */
     fun ResourceTableChunk.getResourceFileName(
         resourceId: BinaryResourceIdentifier,
         configurationName: String,
@@ -151,8 +112,6 @@ object ArscUtil {
             error("Target resource value type is not STRING")
 
         val valueIdx = entry.value().data()
-        val value = this.stringPool.getString(valueIdx)
-
-        return value
+        return this.stringPool.getString(valueIdx)
     }
 }
